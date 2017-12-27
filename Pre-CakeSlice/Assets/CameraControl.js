df8a52c7824fe25645e5fbfe0f15e17f638d05fe
Miko Dimov <mdimov@bu.edu>
2017-12-27 20:30:39 +0100

    added simple unity project with animations imported

---
+++ Pre-CakeSlice/Assets/CameraControl.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var target : Transform;
+var xOffset : float;
+var yOffset : float;
+var zOffset : float;
+
+function LateUpdate () 
+{
+	transform.position = target.position + Vector3(xOffset, yOffset, zOffset);
+	// transform.LookAt (target);
+}
---
+++ Pre-CakeSlice/Assets/LoisControl.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var moveUp : KeyCode;
+var moveDown : KeyCode;
+var moveLeft : KeyCode;
+var moveRight : KeyCode;
+
+var speed : float = 5;
+
+function Update () {
+	if (Input.GetKey(moveUp)) {
+		GetComponent.<Rigidbody2D>().velocity.y = speed;
+		//GetComponent.<Animation>().animations[0].play;
+	} else if (Input.GetKey(moveDown)) {
+		GetComponent.<Rigidbody2D>().velocity.y = speed * -1;
+	} else {
+		GetComponent.<Rigidbody2D>().velocity.y = 0;
+	}
+	
+	if (Input.GetKey(moveRight)) {
+		GetComponent.<Rigidbody2D>().velocity.x = speed;
+	} else if (Input.GetKey(moveLeft)) {
+		GetComponent.<Rigidbody2D>().velocity.x = speed * -1;
+	} else {
+		GetComponent.<Rigidbody2D>().velocity.x = 0;
+	}
+}
